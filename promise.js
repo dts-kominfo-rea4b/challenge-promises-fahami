524571b283775d780787619d6bebeea4044bcf4c
Fahmi <fahmi@ekrut.com>
2022-09-16 15:30:18 +0700

    add promiseOutput function to count cinema goers emotion

--- promise.js
+++ promise.js
@@ -1,8 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+	let count = 0;
+	let results = [];
+	const theaterIXX = await promiseTheaterIXX();
+	const theaterVGC = await promiseTheaterVGC();
+	results = theaterIXX.concat(theaterVGC);
+	for (const film in results) {
+		if (results[film]["hasil"] === emotion) {
+			count++;
+		}
+	}
+	return count;
+};
 
 module.exports = {
-  promiseOutput,
+	promiseOutput,
 };

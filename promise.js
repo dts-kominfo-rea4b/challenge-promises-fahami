1763db006ffee18ec1dba615c0c0dfb12f0d7ae0
Fahmi <fahmi@ekrut.com>
2022-09-16 18:51:56 +0700

    optimize filtering

--- promise.js
+++ promise.js
@@ -3,16 +3,12 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 const promiseOutput = async (emotion) => {
 	let count = 0;
-	let results = [];
+	let result = Array();
 	const theaterIXX = await promiseTheaterIXX();
 	const theaterVGC = await promiseTheaterVGC();
-	results = theaterIXX.concat(theaterVGC);
-	for (const film in results) {
-		if (results[film]["hasil"] === emotion) {
-			count++;
-		}
-	}
-	return count;
+	result = theaterIXX.concat(theaterVGC);
+	const filtered = result.filter((review) => review["hasil"] === emotion);
+	return filtered.length;
 };
 
 module.exports = {
